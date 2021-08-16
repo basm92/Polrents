bed4de55aa90b8224df9233fe3094e3cd5c5d496
basm92 <basmachielsen@live.nl>
2021-08-16 21:25:34 +0200

    add new unmatched samp with vars, and add code to do it

--- Code/Analyses/rdd_analysis.R
+++ Code/Analyses/rdd_analysis.R
@@ -1,10 +1,11 @@
 library(readxl); library(tidyverse); library(rddtools); library(hrbrthemes)
 
-dataset <- read_csv("./Data/analysis/unmatched_sample_analysis.csv") %>%
+dataset <- read_csv("./Data/analysis/unmatched_sample_with_vars.csv") %>%
   select(-1) %>%
   mutate(defw = log(1+Vermogen_deflated),
          distrverk = str_c(District, "-", Verkiezingdatum)) %>%
-  filter(!is.na(defw))
+  filter(!is.na(defw)) %>%
+  filter(margin > -0.15) # Changing this helps the density asymmetry
 
 test <- dataset %>%
   filter(is.na(`b1-nummer`)) %>%
@@ -35,10 +36,14 @@ dataset_analysis <- rdd_data(
 
 bw_ik <- rdd_bw_ik(dataset_analysis)
 
-model1 <- reg_nonpara <- rdd_reg_np(
+model1 <- rdd_reg_np(
   rdd_object=dataset_analysis, bw=bw_ik)
 
+model2<- rdd_reg_lm(rdd_object = dataset_analysis, order = 2)
+
 dens_test(model1)
+dens_test(model2)
+
 plot(dataset_analysis, h = c(0.005, 0.05, 0.1), nplot = 3)
 
 
