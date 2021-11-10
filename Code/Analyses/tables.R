f5f8f3307cfef1fe6589edfa628baca4e2ffa0a9
basm92 <basmachielsen@live.nl>
2021-11-10 20:56:04 +0100

    small start on making tables

---
+++ Code/Analyses/tables.R
@@ -0,0 +1,41 @@
+library(readxl); library(tidyverse); library(hrbrthemes); 
+library(rdrobust); library(modelsummary)
+
+# Parameters
+target_margin_neg <- -0.18 #0.18
+target_margin_pos <- 0.20 #0.20
+maxdate <- '01-01-1928'
+ihs <- function(x) {log(x + sqrt(x^2 + 1))}
+## Few mutations with the data set
+dataset <- read_delim("../test.csv", delim="\t") %>% # %>% #"./Data/analysis/unmatched_sample_with_vars.csv") %>%
+  select(-1) %>%
+  mutate(defw = log(1+Vermogen_deflated),
+         defw2 = ihs(Vermogen_deflated),
+         distrverk = str_c(District, "-", Verkiezingdatum),
+         lifespan = lifespan/365,
+         tenure = as.numeric(str_extract(tenure, "\\d+"))/365,
+         politician_dummy = if_else(!is.na(`b1-nummer`), 1, 0),
+         politician_indic = if_else(!is.na(`b1-nummer`), "Politician", "Non-Politician"),
+         rec_ar = case_when(stringr::str_detect(party_election, "AR|VA|NC|NH") ~ 1, 
+                            is.na(party_election) ~ 0,
+                            TRUE~  0),
+         rec_kath = case_when(stringr::str_detect(party_election, "Ka|KD") ~ 1,
+                              is.na(party_election) ~ 0,
+                              TRUE~ 0),
+         rec_lib = case_when(stringr::str_detect(party_election, "Lib|VL|AH") ~ 1,
+                             is.na(party_election) ~ 0,
+                             TRUE~ 0),
+         rec_soc = case_when(stringr::str_detect(party_election, "Rad|Soc|SDAP|SDP") ~ 1,
+                             is.na(party_election) ~ 0,
+                             TRUE~ 0),
+         elec_type_alg = if_else(election_type == "algemeen", 1, 0),
+         elec_type_else = if_else(election_type != "algemeen", 1, 0),
+         yod = as.numeric(stringr::str_extract(Sterfdatum,"\\d{4}$")),
+         yoe = as.numeric(stringr::str_extract(Verkiezingdatum, "\\d{4}$"))
+  )
+
+rdrobust::rdrobust(y= dataset$defw, x = dataset$margin) %>%
+  summary()
+
+rdrobust::rdbwselect(y = dataset$defw, x = dataset$margin, bwselect = 'msetwo') %>%
+  summary()
